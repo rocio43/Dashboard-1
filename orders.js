9eef7dbee7809da7244992dc60f9afc3baaa7bc0
rocio43 <98287489+rocio43@users.noreply.github.com>
2022-04-17 19:42:03 -0300

    Create orders.js

---
+++ orders.js
@@ -0,0 +1,32 @@
+const order = [
+    {
+        producName: 'Foldable Mini Drone',
+        producNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        producName: 'Foldable Mini Drone',
+        producNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        producName: 'Foldable Mini Drone',
+        producNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        producName: 'Foldable Mini Drone',
+        producNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        producName: 'Foldable Mini Drone',
+        producNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
