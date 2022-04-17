f1b091ff867c343ad3ea33f6ade8029c02cf4d63
rocio43 <98287489+rocio43@users.noreply.github.com>
2022-04-17 20:07:51 -0300

    Create tables.js

---
+++ tables.js
@@ -0,0 +1,32 @@
+const Tables = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lavender ',
+        productNumber: '85631',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone camera drone',
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
