2d05271a0108eba42a58706918afc902afbdd333
rocio43 <98287489+rocio43@users.noreply.github.com>
2022-04-17 19:42:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,21 @@ closeBtn.addEventListener('click',()=>{
 //day-night mode
 themeToggler.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-variables')
-    //themeToggler.querySelector('span').classList.toggle('active');
-    //themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
-    //themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+//table
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = 
+        `
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.paymentStatus}</td>
+        <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'pending' ? 'warning' : 'primary' }">${order.shipping}</td>
+        <td class="primary">Details</td>
+        `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
 })
